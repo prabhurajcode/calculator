9c59281faf8e9969ecb725a682ff0889e221e995
prabhurajcode <prabhurajdnc@gmail.com>
2024-02-15 23:24:43 +0530

    test cases pass

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 let calculatorDiv = document.createElement("div");
-// calculatorDiv.setAttribute("class","calculator");
-// calculatorDiv.setAttribute("id","calculator");
 
-calculatorDiv.innerHTML +=  `
+calculatorDiv.innerHTML += `
+
+<h1 id="title">Simple Calculator</h1>
+<p id="description">A basic calculator for performing arithmetic operations.</p>
 <div class="container">
 <div class ="calculator">
 <input type="text" placeholder="0" id="result"> </input>
-<button onClick="clear()" id="clear">CL</button>
+<button onClick="clearInput()" id="clear">CL</button>
 <button onClick="del()">DEL</button>
-<button onClick="display(%)" id="percentage">%</button>
-<button onClick="display(/)" id="division">/</button>
-<button onClick="display(7)" id="7">7</button>
-<button onClick="display(8)" id="8">8</button>
-<button onClick="display(9)" id="9">9</button>
-<button onClick="display(*)" id="multiply">*</button>
-<button onClick="display(4)" id="4">4</button>
-<button onClick="display(5)" id="5">5</button>
-<button onClick="display(6)" id="6">6</button>
-<button onClick="display(-)" id="subtract" >-</button>
-<button onClick="display(1)" id="1">1</button>
-<button onClick="display(2)" id="2">2</button>
-<button onClick="display(3)" id="3">3</button>
-<button onClick="display(+)" id="add">+</button>
-<button onClick="display(.)" id="dot">0</button>
-<button onClick="display(0)" id="0">0</button>
+<button onClick="display('%')" id="percentage">%</button>
+<button onClick="display('/')" id="division">/</button>
+<button onClick="display('7')" id="7">7</button>
+<button onClick="display('8')" id="8">8</button>
+<button onClick="display('9')" id="9">9</button>
+<button onClick="display('*')" id="multiply">*</button>
+<button onClick="display('4')" id="4">4</button>
+<button onClick="display('5')" id="5">5</button>
+<button onClick="display('6')" id="6">6</button>
+<button onClick="display('-')" id="subtract" >-</button>
+<button onClick="display('1')" id="1">1</button>
+<button onClick="display('2')" id="2">2</button>
+<button onClick="display('3')" id="3">3</button>
+<button onClick="display('+')" id="add">+</button>
+<button onClick="display('.')" id="dot">.</button>
+<button onClick="display('0')" id="0">0</button>
 <button onClick="calculate()" id="equal" class="equal">=</button>
 </div>
 </div>
-`
+`;
 document.body.append(calculatorDiv);
+
+let result = document.getElementById("result");
+
+function display(num) {
+  result.value += num;
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    alert("invalid");
+  }
+}
+
+function clearInput() {
+    result.value = "";
+}
+
+
+
+function del(){
+    console.log('in line 52');
+
+    result.value = result.value.slice(0,-1);
+}
